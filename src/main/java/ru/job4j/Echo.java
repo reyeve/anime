b7553b2a7582ca6d0f8ab6b7fcbe3e684572a5e8
reyeve <yuridze.b@gmail.com>
2023-12-13 12:37:07 +0200

    инфо + студент инфо

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,3 +1,5 @@
 package ru.job4j;
 
-public class Echo { //a
+public class Echo {
+}//a
+
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+public class StudentInfo {
+   static int i = 2;
+    public static void main(String[] args){
+        System.out.println("2*2 = " + i * i);
+        System.out.println("2*2=4");
+        System.out.println("3*2=6");
+    }
+}
